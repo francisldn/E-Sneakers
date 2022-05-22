ef50a7b5ed97fa53f8515a1a6ceef30b7db8e058
Francis_ldn <fnhh04@gmail.com>
2022-05-22 09:50:59 +0100

    remove duplicate main element

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({children}:LayoutProps) {
         <div className="mx-0 lg:mx-10 xl:mx-52">
             <NavBar/>
             <hr className="hidden sm:block sm:border-[#E4E9F2] sm:mt-3.5"/>
-            <main>{children}</main>
+            <div>{children}</div>
         </div>
         </>
     )
